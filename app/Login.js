5af1ab98fb0710c3f522a5536a99291327c99793
vulla sai kiran kumar <vullasaikiran@gmail.com>
2024-09-17 23:51:43 +0530

    Added a common file Icons

--- app/Login.js
+++ app/Login.js
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "../components/ScreenWrapper";
-import Home from "../assets/icons/Home";
 import { theme } from "../constants/theme";
+import Icon from "../assets/icons";
 
 const Login = () => {
   return (
     <ScreenWrapper bg={"white"}>
       <Text>Login</Text>
-      <Home color={theme.colors.rose} strokeWidth={2} />
+      <Icon name={"home"} strokeWidth={2} color={"red"} />
+      <Icon name={"arrowleft"} strokeWidth={2} color={"green"} />
     </ScreenWrapper>
   );
 };
--- assets/icons/index.js
+++ assets/icons/index.js
@@ -1,16 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Home from "./Home";
+import Arrow_left from "./Arrow_left";
+import { theme } from "../../constants/theme";
 
 const icons = {
   home: Home,
+  arrowleft: Arrow_left,
 };
 
-const Icon = () => {
+const Icon = ({ name, ...props }) => {
+  const IconComponent = icons[name];
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <IconComponent
+      height={props.size || 24}
+      width={props.size || 24}
+      strokeWidth={props.strokeWidth || 1.9}
+      color={theme.colors.textLight}
+      {...props}
+    />
   );
 };
 
