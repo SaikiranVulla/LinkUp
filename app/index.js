d508b27393329258d41019eea5ba1e911aa071a3
vulla sai kiran kumar <vullasaikiran@gmail.com>
2024-09-17 23:25:50 +0530

    Created icon index page

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "expo-router";
 import ScreenWrapper from "../components/ScreenWrapper";
 
 const index = () => {
+  useEffect(() => {
+    sampleCalling();
+  }, []);
+
+  const sampleCalling = () => {
+    console.log("sampleCalling");
+  };
+
   const navigation = useRouter();
   return (
     <ScreenWrapper>
